b3e2d5752d303feef4a2ed15393c006dee358e76
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-03-29 20:38:16 -0500

    Add files via upload

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const logggingingin = (req, res, next) => {
+   
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = logggingingin;
+  
