addf1f56c65f51a9e0540c49ebcf6f6aed638e5d
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-03-29 20:39:24 -0500

    Add files via upload

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+module.exports = {
+    format_date: (date) => {
+      
+      return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+      
+      return parseInt(amount).toLocaleString();
+    },
+    get_emoji: () => {
+      const randomNum = Math.random();
+  
+      
+      
+    },
+  };
+  
